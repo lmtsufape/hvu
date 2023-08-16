1e295b21e397e3c730d5e46135694041bd2f6a69
RafaelaFoerster <rafafoerst@gmail.com>
2023-08-16 16:25:22 -0300

    alteração da logo jpg para svg

--- src/app/logo_hvu.js
+++ src/app/logo_hvu.js
@@ -3,6 +3,6 @@ import Image from "next/image";
 
 export function LogoHVU() {
     return (
-        <img src='./layouts/hvulogo.jpg' alt='teste'/>
+        <img src='./layouts/hvulogo.svg' alt='teste'/>
     );
 }
