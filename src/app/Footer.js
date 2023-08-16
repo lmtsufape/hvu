c8acd2e8f66b36b58b860545c9a98043b8931800
DougM244 <douglas.leitemorais@gmail.com>
2023-08-16 16:31:51 -0300

    Footer atualização

--- src/app/Footer.js
+++ src/app/Footer.js
@@ -2,27 +2,31 @@ import React from 'react'
 import styles from './components/footer.module.css'
 import Image from 'next/image'
 
-function Footer(){
-    return(
-        <footer className={styles.backgroundfooter}>
-            <div>
-                <h1>
-                    Rodapé pra testar
-                    </h1>
-                <p>
-                    Só pra testar
-                    <ul>
-                    <Image src="/layouts/LMTSlogo.png" alt="Logo" width={182} height={57}/>
-                    <Image src="/layouts/UFAPElogo.png" alt="Logo" width={100} height={50}/>
-                    <Image src="/layouts/email.svg" alt="Logo" width={100} height={50}/>
-                    <Image src="/layouts/facebook.svg" alt="Logo" width={100} height={50}/>
-                    <Image src="/layouts/instagram.svg" alt="Logo" width={100} height={50}/>
-                    </ul>
-                </p>
-                
+function Footer() {
+    return (
+      <footer className={styles.backgroundfooter}>
+        <div className={styles.footercontainer}>
+            <div className={styles.logohvu}>
+            <Image src="/layouts/HVUlogo.svg" alt="Logo" width={159.38} height={36}/>
             </div>
-        </footer>
-    )
-}
+            <div  className={styles.logosufapelmts}>
+            <Image src="/layouts/UFAPElogo.svg" alt="Logo" width={182.31} height={57.78} />
+            <Image src="/layouts/LMTSlogo.svg" alt="Logo" width={182.31} height={57.78} />
+            </div>
+            <div className={styles.redessociais}>
+            <Image src="/layouts/email.svg" alt="Logo" width={50} height={25} />
+            </div>
+            <div className={styles.redessociais}>
+            <Image src="/layouts/facebook.svg" alt="Logo" width={50} height={25} />
+            </div>
+            <div className={styles.redessociais}>
+            <Image src="/layouts/instagram.svg" alt="Logo" width={50} height={25} />
+            </div>
+            
+        </div>
+      </footer>
+    );
+  }
+  
 
 export default Footer;
