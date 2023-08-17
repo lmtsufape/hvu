d45a70b471fb72ffedffa9cc531a55f80a036eb1
RafaelaFoerster <rafafoerst@gmail.com>
2023-08-17 17:36:11 -0300

    Integrando o Bootstrap

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,17 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import Image from 'next/image'
 import './Footer'
 import Footer from './Footer'
 import Header from './header'
 import LoginPage from './login_page';
 import {Text} from './texto_login_page';
+import {LoginFormulario} from './login_forms';
 
 export default function Home() {
   return (
     <div>
       <Header />
-      <Text />
+      <LoginFormulario />
       <Footer/>
     </div>
   )
