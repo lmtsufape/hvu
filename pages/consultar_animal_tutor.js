6088304a0c2cbc36392b01857a3056fdb7a08f8b
RafaelaFoerster <rafafoerst@gmail.com>
2023-08-31 12:56:57 -0300

    Criando aquela header verde

---
+++ pages/consultar_animal_tutor.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../src/app/components/consultar_animal_tutor.module.css"
+import Image from 'next/image';
+import {Header03} from "../src/app/header"
+import Footer from "../src/app/components/Footer"
+
+function PageConsultarAnimalTutor() {
+    return (
+        <>
+        <div>
+            <Header03 />
+        </div>
+
+        <div class="header">
+
+        </div>
+
+        <div>
+
+        </div>
+
+        <div>
+            <Footer />
+        </div>
+        </>
+    );
+}
+
+export default PageConsultarAnimalTutor
--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,18 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import Image from 'next/image'
-import './components/Footer'
-import Footer from './components/Footer'
+import Image from 'next/image';
+import './components/Footer';
+import Footer from './components/Footer';
 import Link from 'next/link';
 import Pagecadastrotutor from "../../pages/cadastrotutor";
-import Pagelogin from "../../pages/login"
+import Pagelogin from "../../pages/login";
 import Text from "./texto_login_page";
+import PageConsultarAnimalTutor from "../../pages/consultar_animal_tutor";
+import {SegundaHeader02} from "./another_header";
 
 export default function Home() {
   return (
     <div>
-      <Pagecadastrotutor />
+      <SegundaHeader02 />
     </div>
   )
 }
