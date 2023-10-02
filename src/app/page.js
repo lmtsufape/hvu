a07ffd4a27826415997dee19e83e3bb8b525a4dd
RafaelaFoerster <rafafoerst@gmail.com>
2023-10-02 18:26:47 -0300

    nada demais não

--- src/app/page.js
+++ src/app/page.js
@@ -3,12 +3,13 @@ import Image from 'next/image';
 import './components/Footer';
 import Footer from './components/Footer';
 import Link from 'next/link';
-import Pagecadastrotutor from "../../pages/cadastrotutor";
 import Pagelogin from "../../pages/login";
 import PageConsultarAnimalTutor from "../../pages/consultar_animal_tutor";
 import {SegundaHeader02} from "./another_header";
 
 export default function Home() {
+
+  useClient();
   return (
     <div>
       <Pagelogin />
