3ce55eb30c6d1dabebcd769d6e29c208aa829e1f
RafaelaFoerster <rafafoerst@gmail.com>
2023-10-03 19:58:41 -0300

    página de login com erro

--- pages/login.js
+++ pages/login.js
@@ -1,4 +1,8 @@
+"use client";
+
 import React, { useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/router";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Image from 'next/image';
 import {Header02} from '../src/app/header';
@@ -8,21 +12,42 @@ import styles from "../src/app/components/login.module.css"
 import {EntrarGreenButton} from '../src/app/green_button'
 
 function pageLogin() {
+    const [login, setLogin] = useState({
+        email: "",
+        senha: "",
+    });
 
-    useClient();
+    const router = useRouter();
 
-    const [login, setLogin] = useState('');
-    const [senha, setSenha] = useState('');
+    function handleInputChange(event){
+        const { name, value } = event.target;
+        setLogin({...login, [name]: value});
+    };
 
-    const onSubmit = (e) => {
-        e.preventDefault();
-        const data = {
-            login, 
-            senha
-        }
-        console.log(data);
-    }
+    function handleSubmit(event){
+        event.preventDefault();
+        
+        axios.get("http://localhost:3000/tutor", login) //substituir o localhost pelo caminho corerto do back 
+        //na minha cabeça isso vai pegar o que o usuário digitar e ver se existe algum cadastrato no bd, se não houver cadastro com as mesmas informações, retorna o "erro" 
+        .then(response => {
+            console.log(response.data);
 
+            router.push('../pages/agendarconsulta');
+        })
+
+        .catch(error => {
+            if (error.response) {
+                // Erro de resposta do servidor (por exemplo, código de status HTTP não 2xx)
+                console.error('Erro na resposta do servidor:', error.response.data);
+            } else if (error.request) {
+                // Erro de rede (por exemplo, não foi possível fazer a solicitação)
+                console.error('Erro de rede:', error.request);
+            } else {
+                // Erro desconhecido
+                console.error('Erro desconhecido:', error.message);
+            }
+        });
+    };
 
     return (
         <>
@@ -42,21 +67,34 @@ function pageLogin() {
                 <form class={styles.form}>
                     <div class="form-group">
                         <label for="exampleInputEmail1">E-mail</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Seu email" required 
-                            value={login} onChange={(e) => setLogin(e.target.value)} 
+                        <input 
+                            type="email" 
+                            class="form-control" 
+                            id="exampleInputEmail1" 
+                            aria-describedby="emailHelp" 
+                            placeholder="Seu email" 
+                            required 
+                            value={login.email} 
+                            onChange={handleInputChange} 
                         />
                     </div>
                     <div class="form-group">
                         <label for="exampleInputPassword1">Senha</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Senha" required
-                            value={senha} onChange={(e) => setSenha(e.target.value)}
+                        <input 
+                            type="password" 
+                            class="form-control" 
+                            id="exampleInputPassword1" 
+                            placeholder="Senha" 
+                            required
+                            value={login.senha} 
+                            onChange={handleInputChange} 
                         />
                     </div>
                     <button type="button" class="btn btn-link">Esqueceu a senha?</button>
                 </form>
 
                 <div class={styles.button_box}>
-                    <button type="button" class="btn btn-primary" id={styles.entrar_button} onSubmit={onSubmit}>Entrar</button>
+                    <button type="submit" class="btn btn-primary" id={styles.entrar_button}>Entrar</button>
                     <div class={styles.criar_button_box}>
                         <h6>Não possui conta? </h6>
                         <button type="button" class="btn btn-link">Crie agora</button>
