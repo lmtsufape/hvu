48b49d5acd011f03729ac419344d8ad1b469f487
RafaelaFoerster <rafafoerst@gmail.com>
2023-10-19 17:00:53 -0300

    Campo de pesquisa faltando só conectar com api

--- pages/pesquisa.js
+++
@@ -1,9 +0,0 @@
-import CampoPesquisa from "../src/components/CampoPesquisa/campo_pesquisa";
-
-export default function PesquisaPage() {
-    return(
-        <div>
-            < CampoPesquisa />
-        </div>
-    );
-}
--- src/components/CampoPesquisa/campo_pesquisa.js
+++ src/components/CampoPesquisa/campo_pesquisa.js
@@ -1,47 +1,60 @@
-import React, { useMemo, useState } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import styles from './campo_pesquisa.module.css';
-import Image from "next/image";
-
-const animais = [
-  'cachorro',
-  'gato',
-  'cavalo',
-  'vaca',
-]
+import axios from 'axios';
 
 function CampoPesquisa() {
+  const [busca, setBusca] = useState('');
+  const [resultados, setResultados] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  const[busca, setBusca] = useState('');
+  useEffect(() => {
+    if (busca.trim() !== '') {
+      setLoading(true);
+      axios.get(`URL_DA_SUA_API/?q=${busca}`)
+        .then((response) => {
+          setResultados(response.data);
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.error(error);
+          setLoading(false);
+        });
+    } else {
+      setResultados([]);
+    }
+  }, [busca]);
 
-  const animaisFiltrados = useMemo(() => {
-    const lowerBusca = busca.toLowerCase();
-    return animais.filter((animal) =>
-    animal.toLowerCase().includes(lowerBusca)
-    );
-  }, [busca, animais]);
-    console.log(busca);
-    return (
-        <form className={styles.busca}>
-            <input 
-            type="text" 
-            className={styles.texto_busca} 
-            placeholder="Buscar animal"
-            value={busca}
-            onChange={(ev) => setBusca(ev.target.value)}
-          />
-          <button className={styles.button_busca}>
-              <img src='./layouts/icone_busca.svg' alt="Lupa de busca"/> 
-          </button>
+  const handleInputChange = (event) => {
+    const termo = event.target.value;
+    setBusca(termo);
+  };
 
-          <ul>
-            {animaisFiltrados.map((animais) => (
-              <li key={animais}>{animais}</li>
-            ))}
-          </ul>
-        </form>
-    );
+  return (
+    <form className={styles.busca}>
+      <input
+        type="text"
+        className={styles.texto_busca}
+        name='animais'
+        placeholder="Buscar animal"
+        value={busca}
+        onChange={handleInputChange}
+      />
+      <button className={styles.button_busca}>
+        <img src='./layouts/icone_busca.svg' alt="Lupa de busca" />
+      </button>
+
+      {loading && <p>Carregando...</p>}
+
+      {resultados.length > 0 && !loading && (
+        <datalist className={styles.resultados}>
+          {resultados.map((animal) => (
+            <option key={animal.id} value={animal.nome} />
+          ))}
+        </datalist>
+      )}
+    </form>
+  );
 }
 
-export default CampoPesquisa
+export default CampoPesquisa;
