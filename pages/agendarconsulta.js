fb5abb2c33e3223d4b8300591754c66e7a096d5d
DougM244 <douglas.leitemorais@gmail.com>
2023-10-24 17:26:51 -0300

    Tela de agendar consulta

---
+++ pages/agendarconsulta.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import styles from "../src/components/FormularioAgendarRetorno/formularioagendarretorno.module.css"
+import {Header03} from "../src/components/Header/header";
+import Footer from "../src/components/Footer/Footer";
+import AgendarConsulta from "../src/components/AgendarConsulta/calendarioconsulta"
+import Calendario from "../src/components/Calendario/calendario"
+
+function PageAgendarRetorno(){
+    return(
+        <div>
+            <Header03/>
+            <div>
+                <h1 className={styles.titulocadastro}>Agendar Consulta</h1>
+            </div>
+        <AgendarConsulta/>
+        <Footer/>
+        </div>
+    )
+}
+
+export default PageAgendarRetorno;
