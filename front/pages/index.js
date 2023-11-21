923e92cfa54fc63063fce0441c7e1d4561b2e4fb
Rafaela Foerster <rafafoerst@gmail.com>
2023-11-21 15:00:26 -0300

    alterações página de consulta secretario e de login

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,10 +1,35 @@
-import Pagelogin from "../src/components/FormularioLogin/formulariologin";
+import FormularioLogin from "../src/components/FormularioLogin/formulariologin";
+import { Header02 } from "../src/components/Header/header";
+import Text from "../src/components/TextoLogin/texto_login_page";
+import styles from "../src/components/FormularioLogin/login.module.css";
+import Footer from "@/components/Footer/Footer";
 
-export default function Home() {
-  
-  return (
-    <div>
-      < Pagelogin />
-    </div>
-  )
+function PageLogin() {
+    return (
+        <>
+        <div>
+            < Header02 />
+        </div>
+
+        <div className={styles.container}>
+            <div className={styles.container_box}>
+                <div className={styles.text_box}>
+                    < Text />
+                </div>
+
+                <div className={styles.form_box}>
+                    <h1>Entrar</h1>
+                    <forms>
+                        < FormularioLogin/>
+                    </forms>
+                </div>
+            </div>
+        </div>
+        <div>
+            < Footer />
+        </div>
+        </>
+    );
 }
+
+export default PageLogin
