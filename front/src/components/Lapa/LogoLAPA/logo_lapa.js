7746cbd5121c605cbccefc334ebfa8f2255f4492
ricaellenascimento <ricaellenascimento@gmail.com>
2024-01-31 14:39:42 -0300

    Revert "Front do LAPA"
    This reverts commit a438f34f203a5171f0e39b84c6ba90c83bd34a66.

--- front/src/components/Lapa/LogoLAPA/logo_lapa.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Image from "next/image";
-
-export default function LogoLAPA() {
-    return (
-        <img src='/images/logoLAPA.svg' alt='Logo da Patologia'/>
-    );
-}
