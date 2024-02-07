557e937b1c0db0b25e02e2ca4bb462b6fd88cd0f
RafaelaFoerster <rafafoerst@gmail.com>
2024-02-07 12:34:55 -0300

    Alterações nas telas de listagem dos agendamentos para o secretário

--- front/src/components/GetAgendamentosFilter/index.js
+++ front/src/components/GetAgendamentosFilter/index.js
@@ -1,26 +1,32 @@
 import React from 'react';
 import { useRouter } from 'next/router';
-import DropdownButton from 'react-bootstrap/DropdownButton';
-import Dropdown from 'react-bootstrap/Dropdown';
 import styles from './index.module.css';
 
 function Filter() {
   const router = useRouter();
 
   const handlDiaClick = () => {
-      router.push('getAllAgendamentoDiaSecretario');
+    router.push('/getAllAgendamentoDiaSecretario');
   };
 
   const handlSemanaClick = () => {
-    router.push('getAllAgendamentoSemanaSecretario');
+    router.push('/getAllAgendamentoSemanaSecretario');
   };
 
   return ( 
-    <div className={styles.box} >
-      <DropdownButton title="Filtrar" id="bg-nested-dropdown">
-        <Dropdown.Item eventKey="1" onClick={handlDiaClick}>Dia</Dropdown.Item>
-        <Dropdown.Item eventKey="2" onClick={handlSemanaClick}>Semana</Dropdown.Item>
-      </DropdownButton>
+    <div>
+      <select className={styles.filter} onChange={(e) => {
+        const selectedOption = e.target.value;
+        if (selectedOption === 'dia') {
+          handlDiaClick();
+        } else if (selectedOption === 'semana') {
+          handlSemanaClick();
+        }
+      }}>
+        <option value="filtro">Filtro</option>
+        <option value="dia">Dia</option>
+        <option value="semana">Semana</option>
+      </select>
     </div>
   );
 }
