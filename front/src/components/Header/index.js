fe59eb08f819bab5af088db6f6efe4847fec0ab0
RafaelaFoerster <rafafoerst@gmail.com>
2024-02-19 14:40:01 -0300

    Layouts de páginas principais de tutor e secretário, esqueceu senha e sistema,  modal do tutor e correções

--- front/src/components/Header/index.js
+++ front/src/components/Header/index.js
@@ -1,11 +1,10 @@
-import React from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useRouter } from 'next/router';
 import Image from "next/image";
 import styles from "./index.module.css";
 import {LoginGreenButton} from '../GreenButton';
 import { LoginWhiteButton } from '../WhiteButton';
 import {CadastrolWhiteButton} from '../WhiteButton';
-import LogoHVU from '../LogoHVU/logo_hvu';
 
 //Header com botão de login e cadastro
 export function Header01() {
@@ -13,7 +12,7 @@ export function Header01() {
         <header className={styles.header}>
             
             <div className={styles.boxlogo}>
-                < LogoHVU />
+                <Image src="/blacklogohvu.svg" alt="Logo HVU" width={150.38} height={36}/>
             </div>
 
             <div className={styles.box_buttons} >
@@ -33,7 +32,7 @@ export function Header01() {
         <header className={styles.header}>
             
             <div className={styles.boxlogo}>
-                < LogoHVU />
+                <Image src="/blacklogohvu.svg" alt="Logo HVU" width={150.38} height={36}/>
             </div>
 
             <div className={styles.box_buttons} >
@@ -47,22 +46,55 @@ export function Header01() {
 
   //Header com ícone de perfil
   export function Header03() {
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+    const dropdownRef = useRef(null);
+
+    const toggleDropdown = () => {
+        setDropdownOpen(!dropdownOpen);
+    };
+
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+                setDropdownOpen(false);
+            }
+        }
+
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [dropdownRef]);
+
     return (
         <header className={styles.header}>
             
             <div className={styles.boxlogo}>
-                < LogoHVU />
+                <Image src="/blacklogohvu.svg" alt="Logo HVU" width={150.38} height={36}/>
             </div>
 
-            <div className={styles.box_buttons} >
-                <button type="button" className="btn btn-link">
-                    <img src='./images/icone_perfil.svg'/>
+            <div className={styles.box_buttons} ref={dropdownRef}>
+                <button type="button" className="btn btn-link" onClick={toggleDropdown}>
+                <Image src="/icone_perfil.svg" alt="Ícone de perfil" width={59.2} height={59.2}/>
                 </button>
+                {dropdownOpen && (
+                    <div className={styles.dropdown_container}>
+                        <div className={styles.dropdown}>
+                            <button className={styles.button1}>
+                                <div><Image src="/info_icon.svg" alt="Ícone de perfil" width={18.87} height={18.87}/></div>
+                                <div>Meu perfil</div>
+                            </button>
+                            <button className={styles.button2}>
+                                <div><Image src="/left_icon.svg" alt="Ícone de perfil" width={17.88} height={17.88}/></div>
+                                <div>Sair</div>
+                            </button>
+                        </div>
+                    </div>
+                )}
             </div>
-                
         </header>
     );    
-  }
+}
 
   //Header botão de login
   export function Header04() {
@@ -70,7 +102,7 @@ export function Header01() {
         <header className={styles.header}>
             
             <div className={styles.boxlogo}>
-                < LogoHVU />
+                <Image src="/blacklogohvu.svg" alt="Logo HVU" width={150.38} height={36}/>
             </div>
 
             <div className={styles.box_buttons} >
