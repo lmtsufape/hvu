f5fe4ee699db3933e047fcb9ba30683ef9a30c8c
RafaelaFoerster <rafafoerst@gmail.com>
2024-04-26 14:15:54 -0300

    Botão home para tela principal na subheader  e correção dos subheaders de médico e secretário

--- front/pages/agendamentosByMedico/[id].js
+++ front/pages/agendamentosByMedico/[id].js
@@ -2,14 +2,14 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import {Header03} from "../../src/components/Header";
 import Footer from "../../src/components/Footer";
-import { SubHeader01 } from "@/components/SubHeader";
+import { SubHeader02 } from "@/components/SubHeader";
 import AgendamentosByMedico from "../../src/components/AgendamentosByMedico";
 
 export default function AgendamentosByMedicoPage(){
     return(
         <div>
             < Header03 />
-            < SubHeader01 />
+            < SubHeader02 />
             < AgendamentosByMedico />
             < Footer />
         </div>
--- front/src/components/SubHeader/index.js
+++ front/src/components/SubHeader/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import styles from "./index.module.css"
 import { useRouter } from "next/router";
+import { getCurrentUsuario } from '../../../services/userService';
 
 //Sem botão de relatório
 export function SubHeader01 () {
@@ -9,6 +10,7 @@ export function SubHeader01 () {
 
     return (
         <div className={styles.button_box}>
+            <button type="button" className="btn btn-link" id={styles.button_decoration} onClick={(e) => router.push("/mainTutor")}>Home</button>
             <button type="button" className="btn btn-link" id={styles.button_decoration} onClick={(e) => router.push("/meusAgendamentos")}>Agendamentos</button>
             <button type="button" className="btn btn-link" id={styles.button_decoration} onClick={(e) => router.push("/meusAnimais")}>Meus animais</button>
         </div>
@@ -19,10 +21,53 @@ export function SubHeader01 () {
 export function SubHeader02 () {
     const router = useRouter();
 
+    const homeButton = async (e) => {
+        e.preventDefault();
+        try{
+            const userData = await getCurrentUsuario();
+            if(userData.roles && Array.isArray(userData.roles) && userData.roles.includes("secretario")){
+                router.push('/mainSecretario');
+            }else if(userData.roles && Array.isArray(userData.roles) && userData.roles.includes("medico")){
+                router.push('/mainMedico');
+            }
+        }catch(error){
+            console.log(error);
+        }  
+    }
+
+    const agendamentosButton = async (e) => {
+        e.preventDefault();
+        try{
+            const userData = await getCurrentUsuario();
+            if(userData.roles && Array.isArray(userData.roles) && userData.roles.includes("secretario")){
+                router.push('/agendamentosDia');
+            }else if(userData.roles && Array.isArray(userData.roles) && userData.roles.includes("medico")){
+                router.push(`/agendamentosByMedico/${userData.usuario.id}`);
+            }
+        }catch(error){
+            console.log(error);
+        }  
+    }
+
+    const pacientesButton = async (e) => {
+        e.preventDefault();
+        try{
+            const userData = await getCurrentUsuario();
+            if(userData.roles && Array.isArray(userData.roles) && userData.roles.includes("secretario")){
+                router.push('/pacientesBySecretário');
+            }else if(userData.roles && Array.isArray(userData.roles) && userData.roles.includes("medico")){
+                router.push(`/pacientesByMedico/${userData.usuario.id}`);
+            }
+        }catch(error){
+            console.log(error);
+        }  
+    }
+
     return (
         <div className={styles.button_box}>
-            <button type="button" className="btn btn-link" id={styles.button_decoration} onClick={(e) => router.push("/agendamentosDia")}>Agendamentos</button>
-            <button type="button" className="btn btn-link" id={styles.button_decoration} onClick={(e) => router.push("/pacientesBySecretario")}>Pacientes</button>
+            <button type="button" className="btn btn-link" id={styles.button_decoration} onClick={homeButton}>Home</button>
+            <button type="button" className="btn btn-link" id={styles.button_decoration} onClick={agendamentosButton}>Agendamentos</button>
+            <button type="button" className="btn btn-link" id={styles.button_decoration} onClick={pacientesButton}>Pacientes</button>
             {/* <button type="button" className="btn btn-link" id={styles.button_decoration}>Relatórios</button> */}
         </div>
     );
