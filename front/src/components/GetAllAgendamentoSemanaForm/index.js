51d53f54b7969ef6045628bb3f41e32e5a97021e
RafaelaFoerster <rafafoerst@gmail.com>
2024-04-29 23:36:29 -0300

    Aumentanod a quantidade de horários para exibir agendamentos para o secretário

--- front/src/components/GetAllAgendamentoSemanaForm/index.js
+++ front/src/components/GetAllAgendamentoSemanaForm/index.js
@@ -25,7 +25,7 @@ function GetAllAgendamentosSemanaForm() {
 
   const [showAlert, setShowAlert] = useState(false);
 
-  const horarios = ['08:00', '09:00', '10:00', '11:00', '12:00'];
+  const horarios = ["08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14,00", "15,00", "16:00", "17,00", "18:00"];
   const diasDaSemana = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
   const dias = [];
 
