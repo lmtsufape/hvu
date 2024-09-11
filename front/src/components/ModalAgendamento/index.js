d1f74244195ae0cf743558e0bb42d07410e81eeb
RafaelaFoerster <rafafoerst@gmail.com>
2024-09-11 15:50:50 -0300

    Funcionalidade de reagendamento

--- front/src/components/ModalAgendamento/index.js
+++ front/src/components/ModalAgendamento/index.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useRouter } from 'next/router';
 import styles from "./index.module.css";
-import { reagendamento } from '../../../services/agendamentoService';
 
 const ModalAgendamento = ({
   tutor,
@@ -11,46 +11,13 @@ const ModalAgendamento = ({
   setDescricaoCancelamento,
   handleCancelarConsulta
 }) => {
-  const [showReagendarModal, setShowReagendarModal] = useState(false);
-  const [dataHorario, setDataHorario] = useState('');
-  const [novaData, setNovaData] = useState('');
-  const [novoHorario, setNovoHorario] = useState('');
+
+    const router = useRouter();
 
   if (!isOpen) return null;
 
   console.log("selectedVaga:", selectedVaga); 
-  console.log("dataHorario:", dataHorario); 
-  console.log("novaData:", novaData); 
-  console.log("novoHorario:", novoHorario); 
 
-  const handleReagendarVaga = async () => {
-    try {
-      const agendamentoId = selectedVaga?.agendamento?.id;
-      console.log("agendamentoId:", agendamentoId);
-      if (!agendamentoId) {
-        throw new Error("Agendamento ID não encontrado.");
-      }
-
-      // Combinar a nova data e o novo horário no formato desejado
-      const dataHorarioFormatado = new Date(`${novaData}T${novoHorario}:00.000Z`).toISOString();
-      setDataHorario(dataHorarioFormatado);
-
-      await reagendamento(dataHorarioFormatado, agendamentoId);
-      setShowReagendarModal(false);
-      closeModal(); 
-      console.log("Reagendamento concluído com sucesso!")
-    } catch (error) {
-      console.error('Erro ao reagendar vaga:', error);
-    }
-  };
-
-  const handleOpenReagendarModal = () => {
-    setShowReagendarModal(true);
-  };
-
-  const handleCloseReagendarModal = () => {
-    setShowReagendarModal(false);
-  };
 
   return (
     <div>
@@ -87,8 +54,8 @@ const ModalAgendamento = ({
                 />
               </div>
               <div className={styles.div_button2}>
-                <button onClick={handleOpenReagendarModal} className={styles.button_reagendar_consulta}>
-                  Reagendar consulta
+                <button onClick={() => router.push(`/reagendarConsulta/${selectedVaga?.agendamento?.id}`)} className={styles.button_reagendar_consulta}>
+                  Reagendar
                 </button>
                 <button 
                   onClick={handleCancelarConsulta} 
@@ -99,50 +66,6 @@ const ModalAgendamento = ({
               </div>
             </div>
           </div>
-          {showReagendarModal && (
-            <div className={styles.modalOverlay}>
-              <div className={styles.modal}>
-                <div className={styles.modalContent}>
-                  <div className={styles.box}>
-                    <div className={styles.title}>Reagendar Consulta</div>
-                    <input
-                      className={styles.input}
-                      aria-label="Selecione a nova data"
-                      type="date" 
-                      value={novaData}
-                      onChange={(e) => setNovaData(e.target.value)}
-                    />
-                    <select
-                      className={styles.input}
-                      aria-label="Selecione o novo horário"
-                      value={novoHorario}
-                      onChange={(e) => setNovoHorario(e.target.value)}
-                    >
-                      <option value="">Selecione o horário</option>
-                      <option value="08:00">08:00</option>
-                      <option value="09:00">09:00</option>
-                      <option value="10:00">10:00</option>
-                      <option value="11:00">11:00</option>
-                      <option value="13:00">13:00</option>
-                      <option value="14:00">14:00</option>
-                      <option value="15:00">15:00</option>
-                      <option value="16:00">16:00</option>
-                      <option value="17:00">17:00</option>
-                      <option value="18:00">18:00</option>
-                    </select>
-                  </div>
-                  <div className={styles.div_button2}>
-                    <button onClick={handleReagendarVaga} className={styles.button_reagendar_consulta}>
-                      Confirmar Reagendamento
-                    </button>
-                    <button onClick={handleCloseReagendarModal} className={styles.button_cancelar_consulta}>
-                      Cancelar
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          )}
         </div>
     </div>
   );
