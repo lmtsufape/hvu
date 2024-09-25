0d68d68a1671e7dcb7a29511a3e22b8e795e11c8
Igordcs <igorcostamdt@gmail.com>
2024-09-25 10:59:18 -0300

    Feedback de login

--- front/src/components/GetAllAgendamentoDiaForm/index.js
+++ front/src/components/GetAllAgendamentoDiaForm/index.js
@@ -1,25 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 import styles from "./index.module.css";
 import dateStyles from "../Date/index.module.css";
-import Filter from '../GetAgendamentosFilter';
-import { DataCompleta, DataCurta, DiaDaSemana } from '../Date';
-import CalendarGreenIcon from '../CalendarGreenIcon';
-import VoltarButton from '../VoltarButton';
-import { getVagaByDate } from '../../../services/vagaService';
-import { cancelarVaga } from '../../../services/vagaService';
-import { getTutorByAnimal } from '../../../services/tutorService';
-import ModalAgendamento from '../ModalAgendamento';
-import ErrorAlert from '../ErrorAlert';
+import Filter from "../GetAgendamentosFilter";
+import { DataCompleta, DataCurta, DiaDaSemana } from "../Date";
+import CalendarGreenIcon from "../CalendarGreenIcon";
+import VoltarButton from "../VoltarButton";
+import { getVagaByDate } from "../../../services/vagaService";
+import { cancelarVaga } from "../../../services/vagaService";
+import { getTutorByAnimal } from "../../../services/tutorService";
+import ModalAgendamento from "../ModalAgendamento";
+import ErrorAlert from "../ErrorAlert";
 
 function GetAllAgendamentosDiaForm() {
   const router = useRouter();
-  const [dataSelecionada, setDataSelecionada] = useState(new Date(new Date().setHours(0, 0, 0, 0)));
+  const [dataSelecionada, setDataSelecionada] = useState(
+    new Date(new Date().setHours(0, 0, 0, 0))
+  );
   const [modalOpen, setModalOpen] = useState(false);
   const [vagas, setVagas] = useState([]);
   const [selectedVaga, setSelectedVaga] = useState(null);
-  const [tutor, setTutor] = useState('');
-  const [descricaoCancelamento, setDescricaoCancelamento] = useState('');
+  const [tutor, setTutor] = useState("");
+  const [descricaoCancelamento, setDescricaoCancelamento] = useState("");
   const [showAlert, setShowAlert] = useState(false);
 
   const handleDataSelecionada = (novaData) => {
@@ -31,16 +33,16 @@ function GetAllAgendamentosDiaForm() {
     try {
       const cancelamentoData = {
         vaga: {
-          id: selectedVaga.id
+          id: selectedVaga.id,
         },
-        descricao: descricaoCancelamento
-      }
+        descricao: descricaoCancelamento,
+      };
       await cancelarVaga(cancelamentoData);
       closeModal();
       setShowAlert(true);
       fetchData();
     } catch (error) {
-      console.error('Erro ao cancelar vaga:', error);
+      console.error("Erro ao cancelar vaga:", error);
     }
   };
 
@@ -49,10 +51,12 @@ function GetAllAgendamentosDiaForm() {
     setModalOpen(true);
     if (vaga?.agendamento?.animal?.id) {
       try {
-        const tutorSelected = await getTutorByAnimal(vaga.agendamento.animal.id);
+        const tutorSelected = await getTutorByAnimal(
+          vaga.agendamento.animal.id
+        );
         setTutor(tutorSelected);
       } catch (error) {
-        console.error('Erro ao obter tutor:', error);
+        console.error("Erro ao obter tutor:", error);
       }
     }
   };
@@ -70,7 +74,7 @@ function GetAllAgendamentosDiaForm() {
       const VagasData = await getVagaByDate(dataFormatada);
       setVagas(VagasData);
     } catch (error) {
-      console.error('Erro ao buscar vagas:', error);
+      console.error("Erro ao buscar vagas:", error);
     }
   };
 
@@ -78,7 +82,18 @@ function GetAllAgendamentosDiaForm() {
     fetchData();
   }, [dataSelecionada]);
 
-  const horarios = ["08:00", "09:00", "10:00", "11:00", "13:00", "14:00", "15:00", "16:00"];
+  console.log("vagas:: ", vagas);
+
+  const horarios = [
+    "08:00",
+    "09:00",
+    "10:00",
+    "11:00",
+    "13:00",
+    "14:00",
+    "15:00",
+    "16:00",
+  ];
 
   return (
     <div className={styles.pagina}>
@@ -87,17 +102,27 @@ function GetAllAgendamentosDiaForm() {
         <h1>Agendamentos do dia</h1>
         <div className={styles.cadendar_container}>
           <div className={styles.cadendar_box}>
-            <div className={dateStyles.data_completa}>{DataCompleta(dataSelecionada)}</div>
+            <div className={dateStyles.data_completa}>
+              {DataCompleta(dataSelecionada)}
+            </div>
             <CalendarGreenIcon onDataSelecionada={handleDataSelecionada} />
           </div>
           <Filter />
         </div>
         <div className={styles.menu}>
           <div className={styles.button_options}>
-            <button className={styles.button} onClick={(e) => router.push("/agendamentoEspecial")}>
+            <button
+              className={styles.button}
+              onClick={(e) => router.push("/agendamentoEspecial")}
+            >
               Novo agendamento
             </button>
-            <button className={styles.button} onClick={(e) => router.push("/gerenciarVagas")}>Criar vagas</button>
+            <button
+              className={styles.button}
+              onClick={(e) => router.push("/gerenciarVagas")}
+            >
+              Criar vagas
+            </button>
           </div>
         </div>
         <table className={styles.table}>
@@ -106,8 +131,12 @@ function GetAllAgendamentosDiaForm() {
               <th className={styles.column1_line1}></th>
               <th>
                 <div className={styles.line1}>
-                  <div className={dateStyles.dia_da_semana}>{DiaDaSemana(dataSelecionada)}</div>
-                  <div className={dateStyles.curta}>{DataCurta(dataSelecionada)}</div>
+                  <div className={dateStyles.dia_da_semana}>
+                    {DiaDaSemana(dataSelecionada)}
+                  </div>
+                  <div className={dateStyles.curta}>
+                    {DataCurta(dataSelecionada)}
+                  </div>
                 </div>
               </th>
             </tr>
@@ -119,38 +148,72 @@ function GetAllAgendamentosDiaForm() {
                   <th className={styles.time}>{horario}</th>
                   <th className={styles.th}>
                     <div className={styles.cardsJuntos}>
-                      {vagas?.filter(vaga => {
-                        const vagaHora = new Date(vaga.dataHora).getHours();
-                        const horarioHora = parseInt(horario.split(':')[0], 10);
-                        return vaga.dataHora.startsWith(dataSelecionada.toISOString().slice(0, 10)) && vagaHora === horarioHora;
-                      }).map(vaga => {
-                        return (
-                          <button key={vaga.id} className={`${styles.button} ${styles[`button_${vaga.status.toLowerCase()}`]}`} onClick={() => openModal(vaga)}>
-                            <div className={styles.infos_container}>
-                              <div>
-                                <div className={styles.infos_box1}>
-                                  <div className={styles.info1}>
-                                    {vaga.agendamento?.animal ? (
-                                      <>{vaga.agendamento?.animal.nome} &bull; {vaga.tipoConsulta?.tipo}</>
-                                    ) : (
-                                      <>{vaga.medico?.nome}</>
-                                    )}
+                      {vagas
+                        ?.filter((vaga) => {
+                          const vagaHora = new Date(vaga.dataHora).getHours();
+                          const horarioHora = parseInt(
+                            horario.split(":")[0],
+                            10
+                          );
+                          return (
+                            vaga.dataHora.startsWith(
+                              dataSelecionada.toISOString().slice(0, 10)
+                            ) && vagaHora === horarioHora
+                          );
+                        })
+                        .map((vaga) => {
+                          return (
+                            <button
+                              key={vaga.id}
+                              className={`${styles.button} ${
+                                styles[`button_${vaga.status.toLowerCase()}`]
+                              }`}
+                              onClick={() => openModal(vaga)}
+                            >
+                              <div className={styles.infos_container}>
+                                <div>
+                                  <div className={styles.infos_box1}>
+                                    <div className={styles.info1}>
+                                      {vaga.agendamento?.animal ? (
+                                        <>
+                                          {vaga.agendamento?.animal.nome} &bull;{" "}
+                                          {vaga.tipoConsulta?.tipo}
+                                        </>
+                                      ) : (
+                                        <>{vaga.medico?.nome}</>
+                                      )}
+                                    </div>
+                                    <h2
+                                      className={
+                                        styles[
+                                          `status_${
+                                            vaga.status
+                                              ? vaga.status.toLowerCase()
+                                              : ""
+                                          }`
+                                        ]
+                                      }
+                                    >
+                                      {vaga.status === "precriada"
+                                        ? "Pré-criada"
+                                        : vaga.status}
+                                    </h2>
                                   </div>
-                                  <h2 className={styles[`status_${vaga.status ? vaga.status.toLowerCase() : ''}`]}>
-                                    {vaga.status === "precriada" ? "Pré-criada" : vaga.status}
-                                  </h2>
-                                </div>
-                                <div className={styles.infos_box2}>
-                                  <div className={styles.info2}>
-                                    {vaga?.especialidade?.nome}
+                                  <div className={styles.infos_box2}>
+                                    <div className={styles.info2}>
+                                      {vaga?.especialidade?.nome}
+                                    </div>
+                                    <div className={styles.info2}>
+                                      {horario} -{" "}
+                                      {new Date(vaga.dataHora).getHours() + 1}
+                                      :00
+                                    </div>
                                   </div>
-                                  <div className={styles.info2}>{horario} - {new Date(vaga.dataHora).getHours() + 1}:00</div>
                                 </div>
                               </div>
-                            </div>
-                          </button>
-                        );
-                      })}
+                            </button>
+                          );
+                        })}
                     </div>
                   </th>
                 </tr>
@@ -175,7 +238,12 @@ function GetAllAgendamentosDiaForm() {
         handleCancelarConsulta={handleCancelarConsulta}
       />
 
-      {showAlert && <ErrorAlert message="Agendamento cancelado com sucesso!" show={showAlert} />}
+      {showAlert && (
+        <ErrorAlert
+          message="Agendamento cancelado com sucesso!"
+          show={showAlert}
+        />
+      )}
     </div>
   );
 }
