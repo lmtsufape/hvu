0f9a75f4b89c88c708ab5cade457392bc2cbef37
MarcosNascimento46 <mvenicio141@gmail.com>
2024-09-26 18:35:43 -0300

    Alteração no texto de apresentação do sistema e correção de layout

--- front/pages/system/index.js
+++ front/pages/system/index.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { Header02 } from "@/components/Header";
+import { SubHeader } from "../../src/components/SubHeader";
 import SystemForm from "@/components/SystemForm";
 import Footer from "@/components/Footer";
+import "@/styles/styles.css";
+
 
 function SystemPage() {
     return(
-        <div>
+        <div className="divPai">
             < Header02 />
-            < SystemForm />
+            < SubHeader />
+            <div className="flexStyle">
+                <SystemForm />
+            </div>
             < Footer />
         </div>
     );
