a72e2c86cefc4905395a1e37c0089b2867be9d96
MarcosNascimento46 <mvenicio141@gmail.com>
2024-10-07 22:35:38 -0300

    Alteração no texto de apresentação do sistema

--- front/src/components/SystemForm/index.js
+++ front/src/components/SystemForm/index.js
@@ -5,20 +5,22 @@ function SystemForm() {
     return(
         <div className={styles.page}>
             < VoltarButton />
+            
             <div className={styles.container}>
-                <h1>Sistema</h1>
+                <div className={styles.titulo}>
+                    <h1> O Sistema</h1>
+                </div>
                 <div>
                     <h6>
                         Bem-vindo ao site dedicado à gestão do Hospital Universitário Veterinário (HVU) da Universidade Federal do Agreste de Pernambuco (UFAPE).
                     </h6> 
 
                     <h6>
-                    O sistema realiza o agendamento por tutores de animais que necessitem de atendimento 
-                    nas diversas especialidades médico-veterinárias oferecidas no HVU. Além disso, o sistema 
-                    gerencia os históricos de consultas, exames, cirurgias e tratamentos, simplificando o acesso a
-                    os dados de saúde do animal na ocasião de uma nova consulta e novos exames. Assim, o sistema de 
-                    gestão do HVU oferece uma interface clara e transparente ao usuário, a fim de contribuir para uma maior 
-                    rapidez e eficiência no atendimento.
+                    O sistema realiza o agendamento por tutores de animais que necessitem de atendimento nas diversas 
+                    especialidades médico-veterinárias oferecidas no HVU. Além disso, ele gerencia os históricos de consultas, 
+                    exames, cirurgias e tratamentos, simplificando o acesso aos dados de saúde do animal na ocasião de uma nova 
+                    consulta e novos exames. Assim, a plataforma de gestão do HVU oferece uma interface clara e transparente ao usuário, 
+                    a fim de contribuir para uma maior rapidez e eficiência no atendimento.
                     </h6>
 
                 </div>
