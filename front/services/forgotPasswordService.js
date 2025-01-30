5d947004983dc990b88b64f0edfd099ad057d65f
Thiago2704 <thiagoaraujo909090@gmail.com>
2025-01-30 14:53:03 -0300

    Ajustes na pag de esquecer senha

---
+++ front/services/forgotPasswordService.js
@@ -0,0 +1,14 @@
+import api from '../common/http-common-back';
+
+export async function ForgotPassword(email) {
+    localStorage.clear(); 
+    try {
+      const response = await api.post('/api/auth/forgot-password', { email });
+      return response.data;
+    } catch (error) {
+      const errorMessage = error.response
+        ? error.response.data.message || 'Erro ao tentar recuperar senha.'
+        : 'Erro desconhecido. Verifique sua conexão.';
+      throw new Error(errorMessage);
+    }
+  }
