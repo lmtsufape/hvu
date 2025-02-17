655693aa89178169f905223ab12d902f96e490e0
Thiago2704 <thiagoaraujo909090@gmail.com>
2025-02-17 15:43:56 -0300

    Redução de 30min no horário das vagas e da agenda do secretário

--- front/src/components/AgendamentoEspecial/index.js
+++ front/src/components/AgendamentoEspecial/index.js
@@ -230,16 +230,16 @@ function AgendamentoEspecial() {
                 onChange={handleHorarioChange}
               >
                 <option value="">Selecione o horário</option>
-                <option value="08:00">08:00</option>
-                <option value="09:00">09:00</option>
-                <option value="10:00">10:00</option>
-                <option value="11:00">11:00</option>
-                <option value="13:00">13:00</option>
-                <option value="14:00">14:00</option>
-                <option value="15:00">15:00</option>
-                <option value="16:00">16:00</option>
-                <option value="17:00">17:00</option>
-                <option value="18:00">18:00</option>
+                <option value="07:30">07:30</option>
+                <option value="08:30">08:30</option>
+                <option value="09:30">09:30</option>
+                <option value="10:30">10:30</option>
+                <option value="12:30">12:30</option>
+                <option value="13:30">13:30</option>
+                <option value="14:30">14:30</option>
+                <option value="15:30">15:30</option>
+                <option value="16:30">16:30</option>
+                <option value="17:30">17:30</option>
               </select>
               {errors.escolherHorario && (<div className={`invalid-feedback ${styles.error_message}`}>{errors.escolherHorario}</div>)}
             </div>
