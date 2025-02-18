b49ae25d3e73f3f1758c7880ab42bfbbbd75f069
RafaelaFoerster <rafafoerst@gmail.com>
2025-02-18 10:38:50 -0300

    Classes de fichas clínicas

---
+++ back/src/main/java/br/edu/ufape/hvu/model/Ficha.java
@@ -0,0 +1,33 @@
+package br.edu.ufape.hvu.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import java.time.LocalDateTime;
+import jakarta.persistence.*;
+
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString
+public class Ficha {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private long id;
+    private String nome;
+    @Column(columnDefinition = "jsonb")
+    private String conteudo;
+    private LocalDateTime dataHora;
+}
