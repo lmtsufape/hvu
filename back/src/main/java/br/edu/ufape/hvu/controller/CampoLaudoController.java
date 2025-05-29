e52bd224a66a1f0bf1b6a7400ce9777fdecadf06
GENILDOBURGOS <gnldburgos@gmail.com>
2025-05-29 15:05:27 -0300

    refactor(CampoLaudoController): remove blocos try-catch redundantes

--- back/src/main/java/br/edu/ufape/hvu/controller/CampoLaudoController.java
+++ back/src/main/java/br/edu/ufape/hvu/controller/CampoLaudoController.java
@@ -4,18 +4,14 @@ import java.util.List;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
-import org.springframework.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import jakarta.validation.Valid;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeMap;
-
 import br.edu.ufape.hvu.model.CampoLaudo;
 import br.edu.ufape.hvu.facade.Facade;
 import br.edu.ufape.hvu.controller.dto.request.CampoLaudoRequest;
 import br.edu.ufape.hvu.controller.dto.response.CampoLaudoResponse;
-import br.edu.ufape.hvu.exception.IdNotFoundException;
 
 
  
@@ -45,43 +41,30 @@ public class CampoLaudoController {
 	@PreAuthorize("hasAnyRole('MEDICOLAPA', 'SECRETARIOLAPA')")
 	@GetMapping("campoLaudo/{id}")
 	public CampoLaudoResponse getCampoLaudoById(@PathVariable Long id) {
-		try {
-			return new CampoLaudoResponse(facade.findCampoLaudoById(id));
-		} catch (IdNotFoundException ex) {
-			throw new ResponseStatusException(HttpStatus.NOT_FOUND, ex.getMessage());
-		}
+		return new CampoLaudoResponse(facade.findCampoLaudoById(id));
 	}
 
 	@PreAuthorize("hasAnyRole('MEDICOLAPA', 'SECRETARIOLAPA')")
 	@PatchMapping("campoLaudo/{id}")
 	public CampoLaudoResponse updateCampoLaudo(@PathVariable Long id, @Valid @RequestBody CampoLaudoRequest obj) {
-		try {
-			//CampoLaudo o = obj.convertToEntity();
-			CampoLaudo oldObject = facade.findCampoLaudoById(id);
+		//CampoLaudo o = obj.convertToEntity();
+		CampoLaudo oldObject = facade.findCampoLaudoById(id);
 
-			TypeMap<CampoLaudoRequest, CampoLaudo> typeMapper = modelMapper
+		TypeMap<CampoLaudoRequest, CampoLaudo> typeMapper = modelMapper
 													.typeMap(CampoLaudoRequest.class, CampoLaudo.class)
 													.addMappings(mapper -> mapper.skip(CampoLaudo::setId));			
 			
 			
-			typeMapper.map(obj, oldObject);	
-			return new CampoLaudoResponse(facade.updateCampoLaudo(oldObject));
-		} catch (IdNotFoundException ex) {
-			throw new ResponseStatusException(HttpStatus.CONFLICT, ex.getMessage());
-		}
+		typeMapper.map(obj, oldObject);
+		return new CampoLaudoResponse(facade.updateCampoLaudo(oldObject));
 		
 	}
 
 	@PreAuthorize("hasAnyRole('MEDICOLAPA', 'SECRETARIOLAPA')")
 	@DeleteMapping("campoLaudo/{id}")
 	public String deleteCampoLaudo(@PathVariable Long id) {
-		try {
-			facade.deleteCampoLaudo(id);
-			return "";
-		} catch (IdNotFoundException ex) {
-			throw new ResponseStatusException(HttpStatus.CONFLICT, ex.getMessage());
-		}
-		
+		facade.deleteCampoLaudo(id);
+		return "";
 	}
 	
 
