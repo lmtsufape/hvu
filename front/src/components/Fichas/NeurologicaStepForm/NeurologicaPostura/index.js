ad9d0fa55df1c34a892032173550652169ba5c62
Alcielma <alcielmaluzinete@gmail.com>
2025-06-04 16:20:41 -0300

    dados do animal - ficha neurologica

--- front/src/components/Fichas/NeurologicaStepForm/NeurologicaPostura/index.js
+++ front/src/components/Fichas/NeurologicaStepForm/NeurologicaPostura/index.js
@@ -3,33 +3,155 @@ import styles from "./index.module.css";
 import VoltarButton from "../../../VoltarButton";
 import { CancelarWhiteButton } from "../../../WhiteButton";
 import { ContinuarFichasGreenButton } from "@/components/GreenButton";
+import React, { useState, useEffect, useRef } from "react";
+import { getTutorByAnimal } from "../../../../../services/tutorService";
+import { getAnimalById } from '../../../../../services/animalService';
+import { useRouter } from 'next/router';
 
-function FichaNeurologica({formData, handleChange, nextStep, handleCheckboxChange, cleanLocalStorage}) {
+function FichaNeurologica({ formData, handleChange, nextStep, handleCheckboxChange, cleanLocalStorage }) {
 
     const handleSubmit = (event) => {
         event.preventDefault();
         console.log("Formulário válido. Dados prontos para envio:", formData);
-        nextStep(); 
+        nextStep();
     };
+
+     const [loading, setLoading] = useState(true);
+      const router = useRouter();
+      const [animalId, setAnimalId] = useState(null);
+      const [animal, setAnimal] = useState({});
+      const [showButtons, setShowButtons] = useState(false);
+      const [tutor, setTutor] = useState({});
+      const [consultaId, setConsultaId] = useState(null);
     
-    return(
+      useEffect(() => {
+        if (router.isReady) {
+          const id = router.query.fichaId;
+          const animalId = router.query.animalId;
+          if (id) {
+            setConsultaId(id);
+          }
+          if (animalId) {
+            setAnimalId(animalId);
+          }
+        }
+      }, [router.isReady, router.query.fichaId]);
+    
+      useEffect(() => {
+        if (!animalId) return;
+    
+        const fetchData = async () => {
+          try {
+            const animalData = await getAnimalById(animalId);
+            setAnimal(animalData);
+          } catch (error) {
+            console.error('Erro ao buscar animal:', error);
+          }
+    
+          try {
+            const tutorData = await getTutorByAnimal(animalId);
+            setTutor(tutorData);
+          } catch (error) {
+            console.error('Erro ao buscar tutor do animal:', error);
+          } finally {
+            setLoading(false);
+          }
+        };
+    
+        fetchData();
+      }, [animalId]);
+    
+      const formatDate = (dateString) => {
+        const options = { day: '2-digit', month: '2-digit', year: 'numeric' };
+        return new Date(dateString).toLocaleDateString('pt-BR', options);
+      };
+    
+
+    return (
         <div className={styles.container}>
             <VoltarButton />
             <h1>Ficha Neurológica</h1>
             <div className={styles.form_box}>
 
                 <form onSubmit={handleSubmit}>
-                    <button className={styles.dados_ocultos} type="button">
-                        Dados do animal
-                        <span>+</span>
-                    </button>
+                    <div className={styles.box_ficha_toggle}>
+                        <button
+                            type="button"
+                            className={`${styles.toggleButton} ${showButtons ? styles.minimize : styles.expand}`}
+                            onClick={() => setShowButtons(prev => !prev)}
+                        >
+                            Dados do animal
+                        </button>
+                        {showButtons && (
+                            <div className={styles.container_toggle}>
+                                <ul>
+                                    {animal && (
+                                        <li key={animal.id} className={styles.infos_box}>
+                                            <div className={styles.identificacao}>
+                                                <div className={styles.nome_animal}>{animal.nome}</div>
+                                                <div className={styles.especie_animal}>Nome</div>
+                                            </div>
+                                            <div className={styles.form}>
+                                                <div className={styles["animal-data-box"]}>
+                                                    <div className={styles.lista}>
+                                                        <div className={styles.infos}>
+                                                            <h6>Espécie</h6>
+                                                            <p>{animal.raca && animal.raca.especie && animal.raca.especie.nome}</p>
+                                                        </div>
+                                                        <div className={styles.infos}>
+                                                            <h6>Sexo</h6>
+                                                            <p>{animal.sexo}</p>
+                                                        </div>
+                                                        <div className={styles.infos}>
+                                                            <h6>Peso</h6>
+                                                            <p>{animal.peso === 0 || animal.peso === '' ? 'Não definido' : animal.peso}</p>
+                                                        </div>
+                                                    </div>
+
+                                                    <div className={styles.lista}>
+                                                        <div className={styles.infos}>
+                                                            <h6>Raça</h6>
+                                                            <p>{animal.raca && animal.raca.nome}</p>
+                                                        </div>
+                                                        <div className={styles.infos}>
+                                                            <h6>Porte</h6>
+                                                            <p>{animal.raca && animal.raca.porte ? animal.raca && animal.raca.porte : 'Não definido'}</p>
+                                                        </div>
+                                                        <div className={styles.infos}>
+                                                            <h6>Data de nascimento</h6>
+                                                            <p>{animal.dataNascimento ? formatDate(animal.dataNascimento) : 'Não definida'}</p>
+                                                        </div>
+                                                    </div>
+
+                                                    <div className={styles.lista}>
+                                                        <div className={styles.infos}>
+                                                            <h6>Alergias</h6>
+                                                            <p>{animal.alergias ? animal.alergias : 'Não definidas'}</p>
+                                                        </div>
+                                                        <div className={styles.infos}>
+                                                            <h6>Número da ficha</h6>
+                                                            <p>{animal.numeroFicha ? animal.numeroFicha : 'Não definido'}</p>
+                                                        </div>
+                                                        <div className={styles.infos}>
+                                                            <h6>Tutor</h6>
+                                                            <p>{tutor.nome ? tutor.nome : 'Não definido'}</p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </li>
+                                    )}
+                                </ul>
+                            </div>
+                        )}
+                    </div>
 
                     <div className={styles.titulo}>
                         Estado mental
                     </div>
 
                     <div className={styles.column}>
-                        <label>Nível de consciência ( Troco encefálico e córtex - SARA): 
+                        <label>Nível de consciência ( Troco encefálico e córtex - SARA):
                             <textarea name="nivelConsciencia" value={formData.nivelConsciencia} onChange={handleChange} rows="4" cols="50" />
                         </label>
                     </div>
@@ -38,27 +160,27 @@ function FichaNeurologica({formData, handleChange, nextStep, handleCheckboxChang
                             <textarea name="resultadoExame" value={formData.resultadoExame} onChange={handleChange} rows="4" cols="50" />
                         </label>
                     </div>
-            
+
                     <div className={styles.titulo}>
-                         Postura (Circular)
+                        Postura (Circular)
                     </div>
                     <div className={styles.checkbox_container}>
                         {[
                             "Estação", "Cabeça inclinada (D/E)", "Cabeça virada (D/E)", "Queda",
                             "Tetania", "Tremor", "Descerebrada", "Descerebelada",
-                            "Schiff-sherrington", "Decubito",  "Outro"
+                            "Schiff-sherrington", "Decubito", "Outro"
                         ].map((item) => (
                             <label key={item}>
                                 <input
                                     type="checkbox"
                                     value={item}
-                                    checked={formData["postura"]?.includes(item)} 
+                                    checked={formData["postura"]?.includes(item)}
                                     onChange={(e) => handleCheckboxChange(e, "postura")}
                                 /> {item.replace(/([A-Z])/g, ' $1').trim()}
                             </label>
                         ))}
                     </div>
-                    
+
                     <div className={styles.titulo}>
                         Locomoção (Integração entre visão, equilíbrio, propriocepção e função motora)
                     </div>
@@ -67,37 +189,37 @@ function FichaNeurologica({formData, handleChange, nextStep, handleCheckboxChang
                     </div>
                     <div className={styles.checkbox_container}>
                         {[
-                            "Normal", "Claudicação", "Ataxia",  "Paresia"
-                            ].map((item) => (
+                            "Normal", "Claudicação", "Ataxia", "Paresia"
+                        ].map((item) => (
                             <label key={item}>
                                 <input
                                     type="checkbox"
                                     value={item}
-                                    checked={formData["descricaoLocomocao"]?.includes(item)} 
+                                    checked={formData["descricaoLocomocao"]?.includes(item)}
                                     onChange={(e) => handleCheckboxChange(e, "descricaoLocomocao")}
                                 /> {item.replace(/([A-Z])/g, ' $1').trim()}
                             </label>
                         ))}
                     </div>
-                    
+
                     <div className={styles.titulo}>
                         Tipo de ataxia
                     </div>
                     <div className={styles.checkbox_container}>
                         {[
                             "vestibular", "cerebelar", "proprioceptiva"
-                            ].map((item) => (
+                        ].map((item) => (
                             <label key={item}>
                                 <input
                                     type="checkbox"
                                     value={item}
                                     checked={formData["tipoAtaxia"]?.includes(item)}
                                     onChange={(e) => handleCheckboxChange(e, "tipoAtaxia")}
-                                    /> {item.replace(/([A-Z])/g, ' $1').trim()}
+                                /> {item.replace(/([A-Z])/g, ' $1').trim()}
                             </label>
                         ))}
                     </div>
-          
+
                     <div className={styles.column}>
                         <label>Andar compulsivo?</label>
                         <select id="meia-caixa" name="andarCompulsivo" value={formData.andarCompulsivo} onChange={handleChange} >
@@ -108,8 +230,8 @@ function FichaNeurologica({formData, handleChange, nextStep, handleCheckboxChang
                     </div>
 
                     <div className={styles.button_box}>
-                        < CancelarWhiteButton onClick={cleanLocalStorage}/>
-                        < ContinuarFichasGreenButton type="submit"/>
+                        < CancelarWhiteButton onClick={cleanLocalStorage} />
+                        < ContinuarFichasGreenButton type="submit" />
                     </div>
                 </form>
             </div>
