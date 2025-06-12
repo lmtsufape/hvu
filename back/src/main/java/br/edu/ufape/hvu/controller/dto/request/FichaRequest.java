c5f1175adc4053fdd8cb89cf973a7340c1f26613
GENILDOBURGOS <gnldburgos@gmail.com>
2025-06-12 12:24:11 -0300

    fix: corrige erro de parsing JSON causado por conteúdo serializado incorretamente

--- back/src/main/java/br/edu/ufape/hvu/controller/dto/request/FichaRequest.java
+++ back/src/main/java/br/edu/ufape/hvu/controller/dto/request/FichaRequest.java
@@ -1,19 +1,16 @@
 package br.edu.ufape.hvu.controller.dto.request;
 
 import br.edu.ufape.hvu.exception.InvalidJsonException;
-import org.modelmapper.ModelMapper;
-import org.springframework.format.annotation.DateTimeFormat;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import br.edu.ufape.hvu.config.SpringApplicationContext;
 import br.edu.ufape.hvu.model.Ficha;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 import java.util.Map;
-import com.fasterxml.jackson.core.JsonProcessingException;
 
 
 @Getter @Setter @NoArgsConstructor
@@ -25,19 +22,33 @@ public  class FichaRequest {
     @DateTimeFormat(pattern = "dd/MM/yyyy hh:mm")
     private LocalDateTime dataHora;
 
-
     public Ficha convertToEntity() {
-        ModelMapper modelMapper = (ModelMapper) SpringApplicationContext.getBean("modelMapper");
-        Ficha obj = modelMapper.map(this, Ficha.class);
+        Ficha ficha = new Ficha();
+        ficha.setId(this.id);
+        ficha.setNome(this.nome);
+        ficha.setDataHora(this.dataHora);
 
         ObjectMapper objectMapper = new ObjectMapper();
         try {
-            String conteudoJson = objectMapper.writeValueAsString(conteudo);
-            obj.setConteudo(conteudoJson);
+            String conteudoJson = objectMapper.writeValueAsString(this.conteudo);
+            ficha.setConteudo(conteudoJson);
         } catch (JsonProcessingException e) {
             throw new InvalidJsonException("Erro ao converter o campo 'conteudo' para JSON válido.", e);
         }
 
-        return obj;
+        return ficha;
+    }
+
+    public void applyToEntity(Ficha ficha) {
+        ficha.setNome(this.nome);
+        ficha.setDataHora(this.dataHora);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            String conteudoJson = objectMapper.writeValueAsString(this.conteudo);
+            ficha.setConteudo(conteudoJson);
+        } catch (JsonProcessingException e) {
+            throw new InvalidJsonException("Erro ao converter o campo 'conteudo' para JSON válido.", e);
+        }
     }
 }
