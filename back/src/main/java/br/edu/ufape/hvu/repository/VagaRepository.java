8e7f138b8a8ff4736e055f890acc630f5c0c8ff1
GENILDOBURGOS <gnldburgos@gmail.com>
2025-06-19 19:39:00 -0300

    build(vagaRepository): imports necessarios

--- back/src/main/java/br/edu/ufape/hvu/repository/VagaRepository.java
+++ back/src/main/java/br/edu/ufape/hvu/repository/VagaRepository.java
@@ -2,9 +2,12 @@ package br.edu.ufape.hvu.repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import br.edu.ufape.hvu.model.*;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
