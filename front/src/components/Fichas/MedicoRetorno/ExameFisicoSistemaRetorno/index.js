6e486a5a266a42c4aa423d0af3827e12e9d08d8b
MarcosNascimento46 <mvenicio141@gmail.com>
2025-09-11 23:16:33 -0300

    Adiciona nome do medico de forma automatica para ficha de retorno clinico

--- front/src/components/Fichas/MedicoRetorno/ExameFisicoSistemaRetorno/index.js
+++ front/src/components/Fichas/MedicoRetorno/ExameFisicoSistemaRetorno/index.js
@@ -46,6 +46,19 @@ export default function ClinicaMedicaRetornoStep2({
   const [tutor, setTutor] = useState({});
   const [consultaId, setConsultaId] = useState(null);
 
+  const [nomeMedico, setNomeMedico] = useState("Carregando...");
+  useEffect(() => {
+    if (router.isReady) {
+      const medicoFromQuery = router.query.medico;
+
+      if (medicoFromQuery) {
+        setNomeMedico(decodeURIComponent(medicoFromQuery));
+      } else {
+        setNomeMedico("Médico não informado");
+      }
+    }
+  }, [router.isReady, router.query.medico]);
+
   useEffect(() => {
     if (router.isReady) {
       const id = router.query.fichaId;
@@ -213,11 +226,18 @@ export default function ClinicaMedicaRetornoStep2({
           </div>
           <div className={styles.column}>
             <label>Médico(s) Veterinário(s) Responsável:</label>
-            <textarea name="medicosResponsaveis" value={formData.medicosResponsaveis} onChange={handleChange} />
+            <input
+            type="text"
+            name="medicosResponsaveis"
+            value={formData.medicosResponsaveis || ''} 
+            readOnly
+            className="form-control"
+            style={{ backgroundColor: '#e9ecef', cursor: 'not-allowed' }}
+            />
           </div>
           <div className={styles.button_box}>
             <VoltarWhiteButton onClick={prevStep} />
-            <FinalizarFichaModal onConfirm={handleSubmit} />
+            <FinalizarFichaModal onConfirm={()=>handleSubmit(nomeMedico)} />
           </div>
         </form>
 
