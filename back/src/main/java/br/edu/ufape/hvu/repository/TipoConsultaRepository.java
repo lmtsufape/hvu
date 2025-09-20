0beece744dd2af412343b8c4172209718fd4d234
Luis <luishenriqueds01@gmail.com>
2025-09-19 21:54:35 -0300

    fix(tipo-consulta): ajustar CRUD após remoção do atributo cronograma

--- back/src/main/java/br/edu/ufape/hvu/repository/TipoConsultaRepository.java
+++ back/src/main/java/br/edu/ufape/hvu/repository/TipoConsultaRepository.java
@@ -2,12 +2,9 @@ package br.edu.ufape.hvu.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import br.edu.ufape.hvu.model.TipoConsulta;
 
 @Repository
 public interface TipoConsultaRepository extends JpaRepository<TipoConsulta, Long> {
-
-	
 
 }
