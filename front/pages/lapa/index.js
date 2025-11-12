7ce0f3094aa6a6dde75e8e8298f2cbe32589d42c
Nathan Edhuardo <nathantrabson@gmail.com>
2025-11-12 15:05:22 -0300

    feat: Alerta email/senha invalida no login Lapa / ajuste na pagina de meu perfil

--- front/pages/lapa/index.js
+++ front/pages/lapa/index.js
@@ -2,42 +2,55 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import FormularioLogin from '@/components/Lapa/LoginLapa';
 import { Header02 } from '@/components/Lapa/Header';
-import Text from '../../src/components/Lapa/TextLoginLapa';
+import Text from '@/components/Lapa/TextLoginLapa';
 import styles from "@/components/LoginForm/index.module.css";
 import Footer from "@/components/Lapa/Footer";
+import { Slide, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const queryClient = new QueryClient();
 
 function PageLogin() {
-    return (
-        <div>
-            <QueryClientProvider client={queryClient}>
-                <div>
-                    <Header02 />
-                </div>
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div className={styles.pageWrapper}>
+        <Header02 />
 
-                <div className={styles.container}>
-                    <div className={styles.container_box}>
-                        <div className={styles.text_box}>
-                            <Text />
-                        </div>
+        <div className={styles.container}>
+          <div className={styles.container_box}>
+            <div className={styles.text_box}>
+              <Text />
+            </div>
 
-                        <div className={styles.form_box}>
-                            <h1>Entrar</h1>
-                            <form>
-                                <FormularioLogin />
-                            </form>
-                        </div>
-                    </div>
-                </div>
-
-                <div>
-                    <Footer />
-                </div>
-                <ReactQueryDevtools />
-            </QueryClientProvider>
+            <div className={styles.form_box}>
+              <h1>Entrar</h1>
+              <form>
+                <FormularioLogin />
+              </form>
+            </div>
+          </div>
         </div>
-    );
+
+        <Footer />
+      </div>
+
+      {/* O ToastContainer deve estar aqui, isolado */}
+      <ToastContainer
+        position="top-right"
+        autoClose={2500}
+        hideProgressBar={true}
+        newestOnTop={false}
+        closeOnClick
+        transition={Slide}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
 }
 
 export default PageLogin;
