b0a5cfeac58205c2d75e06cf8a95963740e22dd3
Luis <luishenriqueds01@gmail.com>
2026-01-07 21:05:06 -0300

    feat(db): adicionando migration que remove unique e renomeia coluna de ficha de solicitacao de servico

---
+++ back/src/main/resources/db/migration/V15__remover_unique_animal_id_ficha_solicitacao_servico.sql
@@ -0,0 +1,13 @@
+
+ALTER TABLE ficha_solicitacao_servico
+DROP CONSTRAINT "ficha_solicitacao_servico_animal_id)_key";
+
+ALTER TABLE ficha_solicitacao_servico
+DROP CONSTRAINT fk773507hr7k2fbvnr9susyja8j;
+
+ALTER TABLE ficha_solicitacao_servico
+RENAME COLUMN "animal_id)" TO animal_id;
+
+ALTER TABLE ficha_solicitacao_servico
+ADD CONSTRAINT ficha_solicitacao_servico_animal_id_fkey
+FOREIGN KEY (animal_id) REFERENCES animal(id);
