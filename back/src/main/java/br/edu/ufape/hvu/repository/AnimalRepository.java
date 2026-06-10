d6fe713b344327026931458c13258a5c765a8ffa
Luis Henrique <luishenriqueds01@gmail.com>
2026-06-10 07:51:33 -0300

    refactor: removendo imports desnessários e espaços em branco

--- back/src/main/java/br/edu/ufape/hvu/repository/AnimalRepository.java
+++ back/src/main/java/br/edu/ufape/hvu/repository/AnimalRepository.java
@@ -4,10 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-
 import br.edu.ufape.hvu.model.Animal;
-
 import java.util.List;
 
 @Repository
