3ac03dec6f74eb4002562c0bb7aa4aa9e032c52e
Luis Henrique <luishenriqueds01@gmail.com>
2026-09-03 14:54:18 -0300

    refactor(front): ajustar campos de reflexos e postura na ficha neurologica

--- front/src/components/Fichas/UpdateNeurologica/NeurologicaPostura/index.js
+++ front/src/components/Fichas/UpdateNeurologica/NeurologicaPostura/index.js
@@ -138,8 +138,8 @@ function FichaNeurologica({ formData, handleChange, nextStep, handleCheckboxChan
                                                             <p>{animal.alergias ? animal.alergias : 'Não definidas'}</p>
                                                         </div>
                                                         <div className={styles.infos}>
-                                                            <h6>Número da ficha</h6>
-                                                            <p>{animal.numeroFicha ? animal.numeroFicha : 'Não definido'}</p>
+                                                            <h6>Número de prontuário</h6>
+                                                            <p>{animal.codigoProntuario ? animal.codigoProntuario : 'Não definido'}</p>
                                                         </div>
                                                         <div className={styles.infos}>
                                                             <h6>Tutor</h6>
